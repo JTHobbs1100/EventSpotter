06c02b1cea612c373ec1774b6357a9bb0cd105d1
maseelshah22 <98069253+maseelshah22@users.noreply.github.com>
2024-04-22 11:05:39 -0400

    added jquery to turn events gold on homepage

--- web/src/templates/homepage.php
+++ web/src/templates/homepage.php
@@ -13,8 +13,7 @@ https://getbootstrap.com/docs/4.0/components/navbar/
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="author"
-        content="Maseel Shah (dda5us)[Worked on HTML formatting & general page setup] and Jacob Hobbs (fdu5ff)[worked on CSS styling and HTML formatting]">
+    <meta name="author" content="Maseel Shah (dda5us)[Worked on HTML formatting & general page setup] and Jacob Hobbs (fdu5ff)[worked on CSS styling and HTML formatting]">
     <meta name="description" content="Home Page for Event Spotter Website">
     <meta name="keywords" content="Event Spotter, event, spotter, virginia, uva events, uva, 
         uva event spotter, maseel, jacob, shah, hobbs, maseel shah, jacob hobbs,
@@ -27,25 +26,43 @@ https://getbootstrap.com/docs/4.0/components/navbar/
     <!-- Need to figure this out later  -->
     <!-- <meta property="og:image" content="https://cs4640.cs.virginia.edu/> -->
     <!-- <meta property="og:url" content="https://cs4640.cs.virginia.edu/"> -->
-    <meta property="og:description"
-        content="This website will display arts related events happening in the local area.">
+    <meta property="og:description" content="This website will display arts related events happening in the local area.">
     <!-- title of site -->
     <meta property="og:site_name" content="Event Spotter">
 
     <title>Event Spotter</title>
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <link rel="stylesheet" href="styles/main.css">
     <script src="https://cdn.jsdelivr.net/npm/less"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js" integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
 </head>
 
 
 
 
 <body>
+
+<script>
+  $(document).ready(function() {
+    $(".event").hover(
+      function() {
+        let ogColor = $(this).css('color');
+        $(this).data('ogColor', $(this).css('color'));  
+        $(this).css("color", "gold");  
+      }, 
+      function() {
+        $(this).css("color", $(this).data('ogColor'));  
+      }
+    );
+  });
+</script>
+
     <header>
         <?php include("{$GLOBALS["URL"]}/templates/navbar.php"); ?>
     </header>
@@ -62,13 +79,13 @@ https://getbootstrap.com/docs/4.0/components/navbar/
                 featured events
             </h1>
             <div class="featured-events-list">
-                <?php foreach($_SESSION["featuredEvents"] as $event){ ?>
-                <div class="event">
-                    <h2><?= $event["event_name"]?></h2>
-                    <p class="description"><?= $event["event_description"]?>
-                    </p>
-                </div>
-                <?php }?>
+                <?php foreach ($_SESSION["featuredEvents"] as $event) { ?>
+                    <div class="event">
+                        <h2><?= $event["event_name"] ?></h2>
+                        <p class="description"><?= $event["event_description"] ?>
+                        </p>
+                    </div>
+                <?php } ?>
                 <!-- <div class="event">
                     <h2>Event #1</h2>
                     <p>Event Description <a href="?command=eventdetails">see more...</a></p>
@@ -88,8 +105,7 @@ https://getbootstrap.com/docs/4.0/components/navbar/
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 </body>
 
