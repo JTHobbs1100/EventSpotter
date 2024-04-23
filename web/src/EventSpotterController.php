096cfaeb01ed422f5afdb627677e82ef8d946757
maseelshah22 <98069253+maseelshah22@users.noreply.github.com>
2024-04-23 12:19:10 -0400

    have the create account jquery for input, just style is off for password section

--- web/src/EventSpotterController.php
+++ web/src/EventSpotterController.php
@@ -1,13 +1,15 @@
 <?php
 
-class EventSpotterController {
+class EventSpotterController
+{
 
     private $db;
 
     /**
      * Constructor
      */
-    public function __construct($input) {
+    public function __construct($input)
+    {
         $this->db = new Database();
         $this->input = $input;
         // echo print_r($input);
@@ -23,13 +25,14 @@ class EventSpotterController {
      * which command to execute based on the given "command"
      * parameter.  Default is the welcome page.
      */
-    public function run() {
+    public function run()
+    {
         // Get the command
         $command = "example";
         if (isset($this->input["command"]))
             $command = $this->input["command"];
 
-        switch($command) {
+        switch ($command) {
             case "delete_event":
                 //$_SESSION["activePage"] = "myevents";
                 $this->delete_event();
@@ -64,12 +67,11 @@ class EventSpotterController {
                 $this->showEventDetails();
                 break;
             case "create":
-                if(isset($_SESSION["login_status"]) && $_SESSION["login_status"] == true){
-                $_SESSION["activePage"] = "create";
-                $this->showCreate();
-                break;   
-                }
-                else{
+                if (isset($_SESSION["login_status"]) && $_SESSION["login_status"] == true) {
+                    $_SESSION["activePage"] = "create";
+                    $this->showCreate();
+                    break;
+                } else {
                     header("Location: ?command=login");
                     break;
                 }
@@ -82,7 +84,7 @@ class EventSpotterController {
                 //echo $_SESSION["username"];
                 break;
             case "successful_login":
-               // $_SESSION["login_status"]=true;
+                // $_SESSION["login_status"]=true;
                 $this->showSuccessLogin();
                 break;
             case "logout":
@@ -98,7 +100,8 @@ class EventSpotterController {
     /**
      * Show the example page to the user.
      */
-    public function showHomePage() {
+    public function showHomePage()
+    {
         $dataElement = print_r($this->input, true);
         $res = $this->db->query("select * from events");
         $_SESSION["featuredEvents"] = $res;
@@ -107,154 +110,179 @@ class EventSpotterController {
         include("{$GLOBALS["URL"]}/templates/homepage.php");
     }
     // function that returns JSON data
-    public function getEventsJSON(){
+    public function getEventsJSON()
+    {
         $res = $this->db->query("select * from events");
         $jsonEvents = json_encode($res);
         return $jsonEvents;
     }
-    public function showEvents() {
+    public function showEvents()
+    {
         $dataElement = print_r($this->input, true);
         // $_SESSION["allEvents"] = json_decode($this->getEventsJSON(), true);
         // $_SESSION["allEvents"] = $this->sortEvents($_SESSION["allEvents"]);
         include("{$GLOBALS["URL"]}/templates/events.php");
     }
-    public function showEventDetails() {
+    public function showEventDetails()
+    {
         $dataElement = print_r($this->input, true);
         include("{$GLOBALS["URL"]}/templates/event-details.php");
     }
-    public function showCreate() {
+    public function showCreate()
+    {
         $dataElement = print_r($this->input, true);
         include("{$GLOBALS["URL"]}/templates/create.php");
     }
-    public function showLogin($loginMessage = "") {
+    public function showLogin($loginMessage = "")
+    {
         $dataElement = print_r($this->input, true);
         include("{$GLOBALS["URL"]}/templates/login.php");
     }
-    public function showSuccessLogin() {
+    public function showSuccessLogin()
+    {
         $dataElement = print_r($this->input, true);
         include("{$GLOBALS["URL"]}/templates/successful_login.php");
     }
 
 
-    public function loginDatabase() {
+    public function loginDatabase()
+    {
         // User must provide a non-empty name, email, and password to attempt a login
 
         $loginMessage = "";
-        
-        if(isset($_POST["username"]) && !empty($_POST["username"]) &&
-            isset($_POST["password"]) && !empty($_POST["password"])) {
-                
-                // Check if user is in database, by email
-                $res = $this->db->query("select * from login where username = $1;", $_POST["username"]);
 
-                // echo print_r($res);
-                
-                if (empty($res)) {
-                    //$_SESSION["loginMessage"]
-                    $loginMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
+        if (
+            isset($_POST["username"]) && !empty($_POST["username"]) &&
+            isset($_POST["password"]) && !empty($_POST["password"])
+        ) {
+
+            // Check if user is in database, by email
+            $res = $this->db->query("select * from login where username = $1;", $_POST["username"]);
+
+            // echo print_r($res);
+
+            if (empty($res)) {
+                //$_SESSION["loginMessage"]
+                $loginMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
                         User does not exist! New to Event Spotter? Create an account!
                     </div>";
 
-                    $this->showLogin($loginMessage);
-                   
-                } else {
-                    // User was in the database, verify password is correct
-                    // Note: Since we used a 1-way hash, we must use password_verify()
-                    // to check that the passwords match.
-                    if (password_verify($_POST["password"], $res[0]["password"])) {
-                        // Password was correct, save their information to the
-                        // session and send them to homepage?
-                        $_SESSION["login_status"]=true;
-                        
-                        $_SESSION["username"] = $res[0]["username"];
-                        // echo "User found.";
-                        // echo $res[0]["username"];
+                $this->showLogin($loginMessage);
+            } else {
+                // User was in the database, verify password is correct
+                // Note: Since we used a 1-way hash, we must use password_verify()
+                // to check that the passwords match.
+                if (password_verify($_POST["password"], $res[0]["password"])) {
+                    // Password was correct, save their information to the
+                    // session and send them to homepage?
+                    $_SESSION["login_status"] = true;
 
-                        $this->showHomePage();
-                        
-                        return;
-                    } else {
-                        $loginMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
+                    $_SESSION["username"] = $res[0]["username"];
+                    // echo "User found.";
+                    // echo $res[0]["username"];
+
+                    $this->showHomePage();
+
+                    return;
+                } else {
+                    $loginMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
                         Incorrect username or password!
                     </div>";
 
                     $this->showLogin($loginMessage);
-                     
-                    }
                 }
-        } 
+            }
+        }
         // else {
         //     //echo "Name and password are required.";
         //     $this->errorMessage = "Name and password are required.";
         //     echo $this->errorMessage;
         // }
         // If something went wrong, show the welcome page again
-       // $this->showHomePage();
+        // $this->showHomePage();
     }
 
-    public function showCreateAccountPage($accountCreationMessage = "") {
+    public function showCreateAccountPage($accountCreationMessage = "")
+    {
         $dataElement = print_r($this->input, true);
         include("{$GLOBALS["URL"]}/templates/create-account.php");
     }
 
-    public function createAccount(){
+    public function createAccount()
+    {
         // User must provide a non-empty name, email, and password to create an account
         $accountCreationMessage = "";
-        if(isset($_POST["username"]) && !empty($_POST["username"]) &&
-            isset($_POST["password"]) && !empty($_POST["password"])) {
-                $res = $this->db->query("select * from login where username = $1;", $_POST["username"]);
-                if (empty($res)) {
-                    if(!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/", $_POST["password"])){
-                        $accountCreationMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
-                    Password must be 6-16 characters and contain an upper case letter, an upper case letter, and a number!
+        if (
+            isset($_POST["username"]) && !empty($_POST["username"]) &&
+            isset($_POST["password"]) && !empty($_POST["password"])
+        ) {
+            $res = $this->db->query("select * from login where username = $1;", $_POST["username"]);
+            if (empty($res)) {
+                if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/", $_POST["password"])) {
+                    $accountCreationMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
+                    Password must be 6-16 characters and contain an upper case letter, a lower case letter, and a number!
                 </div>";
 
                     $this->showCreateAccountPage($accountCreationMessage);
-                    }else{
-                        // User was not in the database, so we can create an account
-                        // Hash the password
-                        $hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-                        // Insert the user into the database
-                        $this->db->query("insert into login (username, password) values ($1, $2);", $_POST["username"], $hash);
-                        // Save their information to the session and send them to the question page
-                        $_SESSION["login_status"] = true;
-                        $_SESSION["username"] = $_POST["username"];
-                        $this->showHomePage();
-                    }
-                } else {
-                    // User was in the database, so we cannot create an account
-                    //echo "User already exists.";
+                } elseif (strlen($_POST["username"]) < 5) {
                     $accountCreationMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
+                    Username must be at least 5 characters long!</div>";
+                    $this->showCreateAccountPage($accountCreationMessage);
+                } else {
+                    // User was not in the database, so we can create an account
+                    // Hash the password
+                    $hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+                    // Insert the user into the database
+                    $this->db->query("insert into login (username, password) values ($1, $2);", $_POST["username"], $hash);
+                    // Save their information to the session and send them to the question page
+                    $_SESSION["login_status"] = true;
+                    $_SESSION["username"] = $_POST["username"];
+                    $this->showHomePage();
+                }
+            } else {
+                // User was in the database, so we cannot create an account
+                //echo "User already exists.";
+                $accountCreationMessage = "<div class=\"alert alert-danger\" role=\"alert\" style=\"display: inline-block;\">
                     Username already exists!
                 </div>";
 
-                    $this->showCreateAccountPage($accountCreationMessage);
-
-                }
-        } 
-       
+                $this->showCreateAccountPage($accountCreationMessage);
+            }
+        }
     }
 
-    public function submitEvent(){
+    public function submitEvent()
+    {
         // User must provide a non-empty name, email, and password to create an account
-        if(isset($_POST["event_name"]) && !empty($_POST["event_name"]) &&
+        if (
+            isset($_POST["event_name"]) && !empty($_POST["event_name"]) &&
             isset($_POST["event_description"]) && !empty($_POST["event_description"]) &&
             isset($_POST["event_date"]) && !empty($_POST["event_date"]) &&
             isset($_POST["start_time"]) && !empty($_POST["start_time"]) &&
             isset($_POST["end_time"]) && !empty($_POST["end_time"]) &&
-            isset($_POST["event_location"]) && !empty($_POST["event_location"]) && isset($_SESSION["username"])) {
-                
-                // Insert the user into the database
-                $this->db->query("insert into events (event_name, event_description, event_date, start_time, end_time, event_location,username) values ($1, $2, $3, $4, $5, $6, $7);", 
-                        $_POST["event_name"], $_POST["event_description"], $_POST["event_date"], $_POST["start_time"], $_POST["end_time"], $_POST["event_location"], $_SESSION["username"]);
-               
-                header("Location: ?command=events");
-                return;
-        } 
+            isset($_POST["event_location"]) && !empty($_POST["event_location"]) && isset($_SESSION["username"])
+        ) {
+
+            // Insert the user into the database
+            $this->db->query(
+                "insert into events (event_name, event_description, event_date, start_time, end_time, event_location,username) values ($1, $2, $3, $4, $5, $6, $7);",
+                $_POST["event_name"],
+                $_POST["event_description"],
+                $_POST["event_date"],
+                $_POST["start_time"],
+                $_POST["end_time"],
+                $_POST["event_location"],
+                $_SESSION["username"]
+            );
+
+            header("Location: ?command=events");
+            return;
+        }
     }
-    
-    public function logout(){
-        if(isset($_SESSION["login_status"])){
+
+    public function logout()
+    {
+        if (isset($_SESSION["login_status"])) {
             $_SESSION["login_status"] = false;
             unset($_SESSION["username"]);
         }
@@ -264,22 +292,24 @@ class EventSpotterController {
         header("Location: ?command=homepage");
     }
 
-    public function showMyEvents(){
+    public function showMyEvents()
+    {
         $res = $this->db->query("select * from events where username = $1;", $_SESSION["username"]);
         $_SESSION["myevents"] = $res;
         $_SESSION["myevents"] = $this->sortEvents($_SESSION["myevents"]);
         include("{$GLOBALS["URL"]}/templates/myevents.php");
     }
 
-    public function sortEvents($eventArr){
-        
+    public function sortEvents($eventArr)
+    {
+
         array_multisort(array_column($eventArr, 'event_date'), SORT_ASC, array_column($eventArr, 'start_time'), SORT_ASC, array_column($eventArr, 'end_time'), SORT_ASC,  $eventArr);
-        
+
         return $eventArr;
-        
     }
-    public function delete_event(){
-        if(isset($_POST["event_id"]) && !empty($_POST["event_id"]) ){
+    public function delete_event()
+    {
+        if (isset($_POST["event_id"]) && !empty($_POST["event_id"])) {
             $this->db->query("delete from events where event_id = $1;", $_POST["event_id"]);
         }
         header("Location: ?command=myevents");
--- web/src/templates/create-account.php
+++ web/src/templates/create-account.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="author"
-        content="Maseel Shah (dda5us)[Worked on HTML formatting & general page setup] and Jacob Hobbs (fdu5ff)[worked on CSS styling and HTML formatting]">
+    <meta name="author" content="Maseel Shah (dda5us)[Worked on HTML formatting & general page setup] and Jacob Hobbs (fdu5ff)[worked on CSS styling and HTML formatting]">
     <meta name="description" content="Home Page for Event Spotter Website">
     <meta name="keywords" content="Event Spotter, event, spotter, virginia, uva events, uva, 
         uva event spotter, maseel, jacob, shah, hobbs, maseel shah, jacob hobbs,
@@ -20,8 +19,7 @@
     <!-- Need to figure this out later  -->
     <!-- <meta property="og:image" content="https://cs4640.cs.virginia.edu/> -->
     <!-- <meta property="og:url" content="https://cs4640.cs.virginia.edu/"> -->
-    <meta property="og:description"
-        content="This website will display arts related events happening in the local area.">
+    <meta property="og:description" content="This website will display arts related events happening in the local area.">
     <!-- title of site -->
     <meta property="og:site_name" content="Event Spotter">
 
@@ -30,8 +28,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js" integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <link rel="stylesheet" href="styles/main.css">
 </head>
@@ -49,9 +46,17 @@
             <h1 class="page-title"><a class="page-title2" href="?command=create_account_page" style="text-decoration:none;">create account</a></h1> <br>
             <form class="login-form" action="?command=create_account" method="post">
                 <h2 class="field-label-text">username:</h2>
-                <input type="text" class="login-field" name="username" id="user" aria-label="Username" required> <br>
+                <input type="text" class="login-field" name="username" id="user" aria-label="Username" required>
+                <div id="username-msg" class="form-text" style="color:red;"></div>
+                <br>
                 <h2 class="field-label-text">password:</h2>
                 <input type="password" class="login-field" name="password" id="pass" aria-label="Password" required>
+                <div class="row">
+                    <div class="col-xs-12" style="text-align:center;">
+                        <div id="pass-msg" class="form-text" style="color:red;"></div>
+
+                    </div>
+                </div>
                 <br>
                 <!-- add if statement for session to see if logged in so we can make it a log out button instead -->
                 <button class="field-submit-btn" type="submit">create account</button>
@@ -63,12 +68,40 @@
         <br>
         <div class="row">
             <div class="col-xs-12" style="text-align:center;">
+                <div id="pass-msg" class="form-text" style="color:red;"></div>
+
                 <?= $accountCreationMessage ?>
             </div>
         </div>
     </div>
 
     <script>
+        $(document).ready(function() {
+            $('#user').on('click', function() {
+                if ($('#user').val().length < 5) {
+                    $('#username-msg').text('Username should be at least 5 characters long');
+                }
+            });
+
+            $('#user').on('keyup', function() {
+                if ($('#user').val().length >= 5) {
+                    $('#username-msg').text('');
+                }
+            });
+
+            $('#pass').on('click', function() {
+                if ($('#pass').val().length < 6) {
+                    $('#pass-msg').text('Password must be 6-16 characters and contain an upper and lower case letter, and a number!');
+                }
+            });
+
+            $('#pass').on('keyup', function() {
+                if ($('#pass').val().length >= 6) {
+                    $('#pass-msg').text('');
+                }
+            });
+        });
+
         const createHeader = document.querySelector('.page-title2');
 
         //using arrow function
@@ -92,8 +125,7 @@
         );
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 </body>
 
